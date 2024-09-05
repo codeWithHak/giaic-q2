a63b41def15229f9ba0851b3d4ecf604efa8cccf
codeWithHak <huzairahmedkhan@gmail.com>
2024-09-04 22:39:57 -0700

    create mode toggler for console

--- ReactMeta/02-event-handling/src/App.js
+++ ReactMeta/02-event-handling/src/App.js
@@ -1,9 +1,10 @@
 import "./index.css";
+import ModeToggler from "./ModeToggler"
 
 export default function App() {
   return (
     <div className="App">
-      
+      <ModeToggler/>
     </div>
   );
 }
--- ReactMeta/02-event-handling/src/ModeToggler.js
+++ ReactMeta/02-event-handling/src/ModeToggler.js
@@ -1,9 +1,26 @@
-const darkModeOn = true
+let darkModeOn = false
 const darkMode = "dark mode is on"
 const lightMode = "light mode is on"
 
+const handleClick = ()=>{
+        darkModeOn = !darkModeOn
+
+        if (darkModeOn == true){
+            console.log("dark mode is on");
+            
+        }
+        else{
+            console.log("Light mode is on");
+            
+        }
+    
+}
+
 export default function ModeToggler(){
     return(
+        <div>
         <h1>{darkModeOn ? darkMode : lightMode}</h1>
+        <button onClick={handleClick}>Click Me</button>
+        </div>
     )
 }
