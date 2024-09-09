3d934e1a73255663221581be92576758da0c8c2a
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-09-09 12:21:21 +0500

    create a calculator by following tutorial

--- ReactUdemy/1.calculator/src/App.jsx
+++ ReactUdemy/1.calculator/src/App.jsx
@@ -1,32 +1,93 @@
-import Button from "./Button"
-function App(props) {
+import { useState } from "react"
 
- const handleNumber = () =>{
-  alert("Number:")
+import Button from "./Button"
+function App() {
+
+const [calc, setCalc] = useState({
+  current:"0",
+  total:"0",
+  initialVal:true,
+  preOp:""
+})
+
+
+ const handleNumber = (value) =>{
+   let newValue = value
+  if (!calc.initialVal){
+  newValue = calc.current + value
+  }
+  setCalc({
+    current:newValue, total:calc.total, initialVal:false, preOp:calc.preOp
+  })
   }
 
+
+ const handleOperator = (value)=>{
+  const total = doCalculation()
+  setCalc({current:total.toString(), total:total.toString(),initialVal:true, preOp:value})
+ } 
+
+
+ const doCalculation = ()=>{
+  let total = parseInt(calc.total)
+  
+  switch (calc.preOp){
+    case "+":
+      total += parseInt(calc.current)
+      break;
+    case "-":
+      totoal -= parseInt(calc.current)
+      break;    
+    case "x":
+      total *= parseInt(calc.current)
+      break;
+    case "÷":
+      totoal /= parseInt(calc.current)
+      break;    
+    default:
+      total = parseInt(calc.current)
+  }
+    return total
+}
+
+
+const handleClear = ()=>{
+  setCalc({
+    current:"0",
+    total:"0",
+    initialVal:true,
+    preOp:""
+  })
+}
+
+
+
+ const renderDisplay = ()=>{
+  return calc.current
+ }
+  
   return (
     <div className="flex flex-col items-center text-center py-5 text-3xl">
     <h1 className="py-6 ">Calculator</h1>
-    <div id="ouput" className=  " bg-gray-100 w-[288px] rounded text-right px-6 py-4">0</div>
+    <div id="ouput" className=  "bg-gray-100 w-[288px] rounded text-right px-6 py-4">{renderDisplay()}</div>
     <div className="flex items-center justify-center  ">
     <div className="grid grid-cols-4 gap-2 w-max p-5 bg-gray-100">    
-    <Button number="7" onClick={handleNumber}/>
-    <Button number="8"/>
-    <Button number="9"/>
-    <Button id="multiply" number="x"/>
-    <Button number="4"/>
-    <Button number="5"/>
-    <Button number="6"/>
-    <Button id="minus" number="-"/>
-    <Button number="1"/>
-    <Button number="2"/>
-    <Button number="3"/>
-    <Button id="plus" number="+"/>
-    <Button id="clear" number="C"/>
-    <Button number="0"/>
-    <Button id="equals" bg="#0377fc" color="white" number="="/>
-    <Button id="plus" number="÷"/>
+    <Button value="7" onClick={handleNumber}/>
+    <Button value="8" onClick={handleNumber}/>
+    <Button value="9" onClick={handleNumber}/>
+    <Button id="multiply" value="x" onClick={handleOperator}/>
+    <Button value="4" onClick={handleNumber}/>
+    <Button value="5" onClick={handleNumber}/>
+    <Button value="6" onClick={handleNumber}/>
+    <Button id="minus" value="-" onClick={handleOperator}/>
+    <Button value="1" onClick={handleNumber}/>
+    <Button value="2" onClick={handleNumber}/>
+    <Button value="3" onClick={handleNumber}/>
+    <Button id="plus" value="+" onClick={handleOperator}/>
+    <Button id="clear" value="C" onClick={handleClear}/>
+    <Button value="0" onClick={handleNumber}/>
+    <Button id="equals" bg="#0377fc" color="white" value="=" onClick={handleOperator}/>
+    <Button id="plus" value="÷" onClick={handleOperator}/>
     </div>
     </div>
     </div>
