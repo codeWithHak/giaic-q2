b2cdbec757f0e3f45fb97e4693e79b7323d799e7
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-09-17 12:27:51 +0500

    issue in optional chaining of segments

--- NextJs/MyExperiments/02-catch-all-segments/src/app/products/[...details]/page.tsx
+++ NextJs/MyExperiments/02-catch-all-segments/src/app/products/[...details]/page.tsx
@@ -1,13 +1,27 @@
-export default function Detals ({params}:
-   {
-    params:{details:string[]}
-   }
-){
+export default function Detals({ params }: { params: { details: string[] } }) {
+
+    if (parseInt(params.details?.[2] || '0') > 100){
+        return (
+            <h1>Product Id not found</h1>
+        )
+    }
+  if (params.details.length === 3) {
+    return (
+      <div>
+        <h1>Product's Category: {params.details[0]}</h1>
+        <h1>Product: {params.details[1]}</h1>
+        <h1>Id: {params.details[2]}</h1>
+      </div>
+    );
+  } else if (params.details.length === 2){
     return(
         <div>
-            <h1>Product's Category: {params.details[0]}</h1>
-            <h1>Product: {params.details[1]}</h1>
-            <h1>Id: {params.details[2]}</h1>
+        <h1>Product's Category: {params.details[0]}</h1>
+        <h1>Product: {params.details[1]}</h1>
         </div>
     )
-} 
+  }
+  return(
+    <h1>Product's Category: {params.details[0]}</h1>
+  )
+}
