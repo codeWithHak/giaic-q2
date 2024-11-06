78e8191aca159bef862d2759df83e625d7533975
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-11-06 10:19:26 +0500

    completed counter

--- ReactWithHitesh/00-practice-projects/1.counter/src/App.jsx
+++ ReactWithHitesh/00-practice-projects/1.counter/src/App.jsx
@@ -1,27 +1,33 @@
 import { useState } from "react"
 
 function App() {
- 
-const [count, setCount] = useState(0)
-const addValue = ()=>{
-  if (count < 20) {
-  setCount(count+1)
-}}
+  const [count, setCount] = useState(0)
+  const increaseCount = ()=>{
+    setCount(count => count+1)
+    setCount(count => count+1)
 
-const subtractValue = ()=>{
-  if(count > 0) {
-  setCount(count - 1)
-}}
-  return (
-   <div className=" text-white grid place-items-center w-full h-screen bg-slate-700">
-    <div className="space-y-4 text-center">
-   <h1 className="font-semibold text-5xl bg-slate-900 p-5 rounded-xl">Count: {count}</h1>
-   <button onClick={addValue} className="font-medium py-2 px-5 rounded-full bg-white text-slate-900 m-5" >Add +</button>
-   <br />
-   <button onClick={subtractValue} className="font-medium  py-2 px-5 rounded-full bg-white text-slate-900 m-5">Subtract -</button>
-   </div>
-   </div>
-  )
+//  Iterview Question: What will be the ouput ?
+    // setCount(count => count+1)
+    // setCount(count => count+1)
+    // setCount(count => count+1)
+    
+  }
+  const decreaseCount = ()=>{
+    setCount(count-1)
+  }
+return(
+  <div className="w-4/5 mx-auto">
+    <div className="text-center mt-12 ">
+      <h1 className="text-3xl">Counter {count}</h1>
+      <div className="flex justify-center mt-12 gap-4">
+        <button className="bg-slate-400 px-2 py-1 rounded-sm text-white" onClick={increaseCount}>Add + </button>
+        <button  onClick={decreaseCount}>Subtract -</button>
+
+      </div>
+    </div>
+  </div>
+) 
+
 }
 
 export default App
