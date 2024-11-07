c78a587dd2005d2c48b7a858961a230632f3ee41
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-11-07 10:47:20 +0500

    completed bg changer dynamically

--- ReactWithHitesh/00-practice-projects/2.bg-changer/src/App.jsx
+++ ReactWithHitesh/00-practice-projects/2.bg-changer/src/App.jsx
@@ -1,23 +1,69 @@
-import { useState } from "react"
+
+import React, { useState } from 'react'
 
 function App() {
-
-  const [color,setColor] = useState("green")
-
-
+  const [color,setColor] = useState("blue")
+  
+  
+  function changeColor(e){
+    setColor(e.target.value)
+    console.log(e.target.value);
+    
+    }
   return (
-  <div className= "w-full h-screen" style={{backgroundColor:color}}>
-    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0  px-2">
-      <div className="flex flex-wrap gap-3 justify-center px-4 py-3 shadow-2xl bg-white rounded-3xl font-normal text-xl text-white">
-        <button onClick={()=>setColor("black")} style={{backgroundColor:"black"}} className=" py-2 px-4 rounded-full">Black</button>
-        <button onClick={()=>setColor("red")} style={{backgroundColor:"red"}} className=" py-2 px-4 rounded-full">Red</button>
-        <button onClick={()=>setColor("blue")} style={{backgroundColor:"blue"}} className=" py-2 px-4 rounded-full">Blue</button>
-        <button onClick={()=>setColor("green")} style={{backgroundColor:"green"}} className=" py-2 px-4 rounded-full">Green</button>
-          </div>
-        </div>
-  </div>
-
+    <div className='h-screen w-full' style={{backgroundColor:color}} >
+      <div className='flex w-[80% mx auto] justify-center items-end h-screen pb-12 gap-3'>
+    <button className='btn text-white bg-blue-400  shadow-lg  px-6 py-2 rounded-full'  value='blue' onClick={changeColor}>Blue</button>
+    <button className='btn text-white bg-green-400 shadow-lg  px-6 py-2 rounded-full'  value='green' onClick={changeColor}>Green</button>
+    <button className='btn text-white bg-red-400 shadow-lg  px-6 py-2 rounded-full'  value='red' onClick={changeColor}>Red</button>
+      </div>
+    </div>
   )
 }
 
 export default App
+
+
+// bad and robust code -- Non Dynamic
+
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [color, setColor] = useState("green");
+//   function bgGreen() {
+//     return setColor("green");
+//   }
+//   function bgBlue() {
+//     return setColor("blue");
+//   }
+//   function bgRed() {
+//     return setColor("red");
+//   }
+//   return (
+//     <div className={`h-screen w-full bg-[${color}]`}>
+//       <div className="flex w-[80% mx auto] justify-center items-end h-screen pb-12 gap-3">
+//         <button
+//           className="btn text-white bg-red-400 shadow-lg  px-6 py-2 rounded-full"
+//           onClick={bgRed}
+//         >
+//           Red
+//         </button>
+//         <button
+//           className="btn text-white bg-green-400 shadow-lg  px-6 py-2 rounded-full"
+//           onClick={bgGreen}
+//         >
+//           Green
+//         </button>
+//         <button
+//           className="btn text-white bg-blue-400  shadow-lg  px-6 py-2 rounded-full"
+//           onClick={bgBlue}
+//         >
+//           Blue
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
